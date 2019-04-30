5aa17208b7e508dd0f7e9953f945227562d6e9af
pduncan08 <48493306+pduncan08@users.noreply.github.com>
2019-04-30 13:01:40 -0700

    Add files via upload

---
+++ Wk3_Sec4_Ex3_Lists.py
@@ -0,0 +1,16 @@
+# Python list is created by setting a variable equal
+# to a square bracket set
+
+job_applicants = ["John", "Geoff", "Brady", "Thomas"]
+heights=[61,70,67,64,65]
+int_and_strings=[1,2,3,'four','five']
+empty_list=[]
+
+print(job_applicants)
+def text(print_this):
+    print(print_this)
+text("hello")
+
+#Output
+# ["John", "Geoff", "Brady", "Thomas"]
+# "hello"
