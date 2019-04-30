5a4046e7a3498f598bd1a56fe4a299aeb1736d4c
Paul Duncan <48493306+pduncan08@users.noreply.github.com>
2019-04-30 14:13:01 -0700

    Add files via upload

---
+++ Wk3_Sec4_Ex7.py
@@ -0,0 +1,18 @@
+# Use zip to create a list of dog owners and their
+# dog's names.
+
+names=["Jenny", "Alexus", "Sam", "Grace"]
+dogs_names=["Elphonse","Dr. Doggy DDS", "Carter", "Ralph"]
+names_and_dogs_names=zip(names,dogs_names)
+print(names_and_dogs_names)
+
+#
+an_empty_list=[]
+an_empty_list.append("something")
+print(an_empty_list)
+
+orders=["daisies","periwinkle"]
+print(orders)
+orders.append('tulips')
+orders.append('roses')
+print(orders)
