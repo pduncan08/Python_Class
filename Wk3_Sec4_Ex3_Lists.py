dfeb603235439240d1003a71bcf36cf706983414
Paul Duncan <48493306+pduncan08@users.noreply.github.com>
2019-04-30 13:21:44 -0700

    Update Wk3_Sec4_Ex3_Lists.py

--- Wk3_Sec4_Ex3_Lists.py
+++ Wk3_Sec4_Ex3_Lists.py
@@ -16,3 +16,11 @@ text("hello")
 #Output
 # ["John", "Geoff", "Brady", "Thomas"]
 # "hello"
+
+# By Access elements of a list by adding square brackets
+# with desired element
+print(job_applicants[1])
+print(job_applicants[2])
+
+#to print John - ask for 1 less than what you want
+print(job_applicants[0])
