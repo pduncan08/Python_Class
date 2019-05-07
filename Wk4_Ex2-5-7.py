7f2be3ed3ae6b056c39bb9612c7e59bf8b71a3b1
Paul Duncan <48493306+pduncan08@users.noreply.github.com>
2019-05-07 13:01:40 -0700

    Add files via upload

---
+++ Wk4_Ex2-5-7.py
@@ -0,0 +1,29 @@
+empty_dict={}
+subtotal_to_total={20:24,10:12,5:6,15:18}
+person={"name":"Shuri","age":18, "siblings":["T'Chaka","Ramonda"]}
+coolness={"forest":21,"movies":23,"Tech_Shop":110,"ice_cream_shop":100}
+print (empty_dict)
+print (subtotal_to_total)
+print (person)
+print (coolness)
+
+#powers={[1,2,4,8,16]:2,[1,3,9,27,81]:3}
+#print (powers)
+
+my_dict={}
+my_dict["new-key"]="new-value"
+menu={"oatmeal":3,"avacado toast":6,"carrot Juice":5, "blueberry muffin":2}
+print (menu)
+menu["cheesecake"]=8
+print(menu)
+
+animals={}
+animals={"david":1,"python":3,"penguin":8}
+print (animals)
+
+user_id={}
+user_id={"teraCoder":9018293,"promatchProgramer":119238}
+user_id["cheesecakeEater"]=19
+print (user_id)
+
+         
