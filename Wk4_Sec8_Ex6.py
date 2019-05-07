3549e29de8f71d4e9a2672dfc4c27e8a4274789e
Paul Duncan <48493306+pduncan08@users.noreply.github.com>
2019-05-07 14:12:06 -0700

    Add files via upload

---
+++ Wk4_Sec8_Ex6.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+current_time=datetime.now()
+print(current_time)
+
+print()
+
+import math
+math.pi
+print(math.pi)
+
+print()
+
+import datetime
+print(datetime.datetime(2019,5,7).weekday())
+
+print()
+
+import random
+print(random.randint(1,100))
+
+print ("break")
+import random
+def random_number():
+    number = random.randint(1,10)
+    print(number)
+random_number()
+
+import simplegui
+import codeskulptor
+sound = simplegui.load_sound(codeskulptor.file2url('assets_Epoq-Lepidoptera.ogg'))
+sound.play()
+                             
+
